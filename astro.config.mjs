1f169b57bec8c5e1a16f364386bff27b48db4c3d
Kun Yan <kyan@redhat.com>
2024-06-06 23:17:02 +0800

    fix code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        port: 8080
-    }
+  server: {
+    port: 8080
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
